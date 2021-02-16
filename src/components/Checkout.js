5bf30b4f43577acec4cf718a081f92d41ab3285c
bianca Gonçalves <biancagoncalves@biancas-iMac.local>
2021-02-15 20:57:35 -0800

    switched to yarn, getting too many npm errors, deployed app

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -5,7 +5,7 @@ import { useStateValue } from "../StateProvider";
 import Subtotal from "./Subtotal";
 
 function Checkout() {
-  const [{ basket }, dispatch] = useStateValue();
+  const [{ basket, user }, dispatch] = useStateValue();
 
   return (
     <div className="checkout">
@@ -16,6 +16,7 @@ function Checkout() {
           alt=""
         />
         <div>
+          <h3>Hello, {user?.email}</h3>
           <h2 className="checkout__title">Your Shopping Basket</h2>
         </div>
 
